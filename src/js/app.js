977d0d453e94b0757d7d7243300509e24f375102
Felipe Hanashiro <felipe.hanashiro@exla.com.br>
2014-10-28 22:03:31 -0200

    landscape article s image, starting login page

--- src/js/app.js
+++ src/js/app.js
@@ -6,16 +6,21 @@ var app = angular.module('InfoBox', [
   'mobile-angular-ui',
 	'infoboxApp.controllers.Main',
 	'infoboxApp.services.Analytics',
-	'infoboxApp.controllers.Account'
+	'infoboxApp.controllers.Account',
+	'infoboxApp.controllers.Login'
 ])
 
 app.config(function($routeProvider, $locationProvider) {
-  $routeProvider.when('/',          {templateUrl: "list.html"});
+ $routeProvider.when('/',          {templateUrl: "list.html"});
 	$routeProvider.when('/list',		{templateUrl: "list.html"});
 	$routeProvider.when('/show',		{templateUrl: "show.html"});
 	$routeProvider.when('/account', {
 		templateUrl: "account.html",
 		controller: "AccountCtrl"
+	});
+	$routeProvider.when('/login', {
+		templateUrl: "login.html",
+		controller: "LoginController"
 	});
 	$routeProvider.when('/about', {templateUrl: "about.html"});
 	$routeProvider.otherwise({redirectTo: '/'});
