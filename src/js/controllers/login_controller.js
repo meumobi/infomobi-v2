c46e1b3b58b87f10979c7e3d6e8e094e92bb9319
victor.dias <victor.dias@ipanemax.com>
2014-11-10 19:53:49 -0200

    set bootstrap 3.2 to prevent layout bugs

--- src/js/controllers/login_controller.js
+++ src/js/controllers/login_controller.js
@@ -4,16 +4,7 @@ angular.module('infoboxApp.controllers.Login', [])
 
 .controller('LoginController', function($rootScope, $scope){
 
-  $rootScope.$on("$routeChangeStart", function(){
-    $rootScope.loading = true;
-  });
-
-  $rootScope.$on("$routeChangeSuccess", function(){
-    $rootScope.loading = false;
-  });
-  
-  $scope.teste = "Hello";
-
+	$rootScope.loading = false;
 });
 
  
