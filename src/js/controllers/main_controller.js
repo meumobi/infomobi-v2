bf489f7f79fa4cf6671cf744a0e76a41c1656099
victor.dias <victor.dias@ipanemax.com>
2014-11-21 15:31:18 -0200

    remove non used icons and screens, show spinner during loading content and translate english wording

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -4,14 +4,6 @@ angular.module('infoboxApp.controllers.Main', [])
 
 .controller('MainController', function($rootScope, $scope, $location){
 
-	$rootScope.$on("$routeChangeStart", function(){
-		$rootScope.loading = true;
-	});
-
-	$rootScope.$on("$routeChangeSuccess", function(){
-		$rootScope.loading = false;
-	});
-
 	$scope.userAgent =  navigator.userAgent;
 	
 	$scope.mailto = function(e) {
