5d4638a6a0839ef6c7a871398589d82099a4edb2
Felipe Hanashiro <felipe.hanashiro@exla.com.br>
2014-11-27 21:04:48 -0200

    created utils services

---
+++ src/js/services/meumobi-utils.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('meumobi.utils', [])
+
+.factory('AppUtils', function() {
+	var app = {
+		CanvasImg : (function(){
+		    return{
+		        createBase64Image : function(url,callback){
+		            var img = new Image;
+		            img.onload = function(){
+		                CanvasImg.imgToBase64(img,callback);
+		            }
+		            img.src = url;
+		        },
+		        imgToBase64 : function(img,callback){
+		            var canvas = document.createElement("canvas");
+		            canvas.width = img.width;
+		            canvas.height = img.height;
+		            canvas.getContext("2d").drawImage(img,0,0);
+		            var base64 = canvas.toDataURL();
+		            if(callback){
+		                callback(base64);
+		            }else{
+		                return base64;
+		            }
+		        }
+		    }
+		})();
+	};
+	
+	
+
+	return app;
+})
