962c2ad81cea63beb00665de5df4ba7fad6e45d5
Felipe Hanashiro <felipe.hanashiro@exla.com.br>
2014-12-12 20:44:52 -0200

    images src 404 fixed

--- src/js/services/meumobi-settings.js
+++ src/js/services/meumobi-settings.js
@@ -4,7 +4,7 @@ angular.module('meumobi.settings', [])
 
 .constant("SITE", {
   "DOMAIN": "infobox.meumobi.com",
-  "SRC_URL": "http://meumobi.com",
+  "SRC_URL": "http://meumobi.com/",
   "TIMEOUT": "5000",
   "ITEMS_PER_PAGE": "10",
   "API_URL": "http://meumobi.com/api/"
