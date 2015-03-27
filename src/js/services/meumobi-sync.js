42b81c85377c09cf05e52846b0568d2522b98091
victor.dias <victor.dias@ipanemax.com>
2015-03-27 18:22:36 -0300

    indent meumobi-sync.js

--- src/js/services/meumobi-sync.js
+++ src/js/services/meumobi-sync.js
@@ -6,51 +6,48 @@ angular.module('meumobi.sync', ['meumobi.api','meumobi.appInfo', 'meumobi.utils'
 
 	var app = {
 		get : function(callback){
-		if(window.navigator.onLine) {
-			console.log("Device is online.");
-			app.list(callback); // Online API Request
-		} else {
-			console.log("Device is offline.");
-		    callback({error:"Não foi possível sincronizar a lista de notícias. Verifique suas conexões e tente novamente."}, false);
-		}
-
-		AppFunc.eraseNotifications();
+			if(window.navigator.onLine) {
+				console.log("Device is online.");
+				app.list(callback); // Online API Request
+			} else {
+				console.log("Device is offline.");
+				callback({error:"Não foi possível sincronizar a lista de notícias. Verifique suas conexões e tente novamente."}, false);
+			}
+			AppFunc.eraseNotifications();
 		},
 		list : function(callback){
 			API.Items.latest(
 				function(data, status, getResponseHeaders) {
 					var headers = getResponseHeaders();
-
 					app.setETag(headers.etag);
 					var news = data.items;
 					var imagesUrls = app.getImagesFromNews(news);
 					app.deleteImages();
 					app.saveAllImages(imagesUrls,function(){
-							localStorage['newsList'] = JSON.stringify(news);
-							$rootScope.newsList = news;
-							callback(news, true);
+						localStorage['newsList'] = JSON.stringify(news);
+						$rootScope.newsList = news;
+						callback(news, true);
 					});
 				},
 				function(errorResponse, status) {
 					console.log(status);
 					console.log("Request Failed:" + errorResponse);
 					switch(status){
-						case 304:
-							callback({error:"304"}, false); //No changes in the list
-							break;
-						case 401:
-							delete localStorage.userToken;
-							delete localStorage.mail;
-							$rootScope.go('/login');
-							break;
-						default:
-							if(errorResponse == null){
-								errorResponse = "Houve um problema ao sincronizar. Tente novamente.";
-							}
-							callback({error:status + " - " + errorResponse}, false);
-							break;
+					case 304:
+						callback({error:"304"}, false); //No changes in the list
+						break;
+					case 401:
+						delete localStorage.userToken;
+						delete localStorage.mail;
+						$rootScope.go('/login');
+						break;
+					default:
+						if(errorResponse == null){
+							errorResponse = "Houve um problema ao sincronizar. Tente novamente.";
+						}
+						callback({error:status + " - " + errorResponse}, false);
+						break;
 					}
-
 					$rootScope.loading = false;
 				}
 			);
@@ -70,7 +67,8 @@ angular.module('meumobi.sync', ['meumobi.api','meumobi.appInfo', 'meumobi.utils'
 			}
 			return images;
 		},
-		saveImage : function(image, callback){console.log(AppUtils)
+		saveImage : function(image, callback){
+			console.log(AppUtils)
 			AppUtils.CanvasImg.createBase64Image(image.url,function(img64){
 				localStorage['image_'+image.id] = img64;
 				callback('image_'+image.id, img64);
@@ -85,22 +83,20 @@ angular.module('meumobi.sync', ['meumobi.api','meumobi.appInfo', 'meumobi.utils'
 					//totalImages--;
 					//if(imagesToSave == totalImages){
 						callback();
-					//}
-				//});
-			}
-
-		},
-		deleteImages : function(){
-			for( prop in localStorage ){
-			   if(prop.indexOf('image_')!=-1){
-			   		delete localStorage[prop];
-			   }
-			}
-		},
-		setETag: function(etag) {
-			localStorage['ETag'] = etag;
-		}
-	};
-	return app;
-
-});
+						//}
+						//});
+					}
+				},
+				deleteImages : function(){
+					for( prop in localStorage ){
+						if(prop.indexOf('image_')!=-1){
+							delete localStorage[prop];
+						}
+					}
+				},
+				setETag: function(etag){
+					localStorage['ETag'] = etag;
+				}
+			};
+			return app;
+		});
