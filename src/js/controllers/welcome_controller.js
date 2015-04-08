5bec3e38f5874785afd9e29d7c5774163cb5d6bd
victor.dias <victor.dias@ipanemax.com>
2015-04-08 04:58:58 -0300

    if logged in show menu

--- src/js/controllers/welcome_controller.js
+++ src/js/controllers/welcome_controller.js
@@ -7,5 +7,5 @@ angular
 	function WelcomeController($rootScope, $scope, $routeParams, WelcomeImages) {
 		$rootScope.loading = false;
 		$scope.item = WelcomeImages.get();
-
+		$scope.loggedIn = $rootScope.user ? $rootScope.user.token : false;
 	}
