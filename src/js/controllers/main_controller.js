712e67eaf5383ed798851d2e7cec0dcf354be204
victor dias <victor.dias@meumobi.com>
2017-03-30 13:27:04 -0300

    FIX: Closes #256, SyntaxError: Strict mode does not allow function declarations in a lexically nested statement

--- src/js/controllers/main_controller.js
+++ src/js/controllers/main_controller.js
@@ -5,11 +5,9 @@
   .module('infoMobi')
   .controller('MainController', MainController);
 
-  function MainController($rootScope, $location, AuthService, UtilsService, $log, APP, meuAnalytics, deviceReady, API) {
+  function MainController($rootScope, AuthService, $log, meuCordova, meuUtils, meuCloud, Site) {
   
-    var vm = this;
-
-    vm.logout = function() {
+    $rootScope.logout = function() {
       AuthService.logout();
       $rootScope.flip('#/login');
     }
@@ -19,62 +17,52 @@
     */
     $rootScope.$on('$routeChangeSuccess', function(e, current, prev) {
       if (current.$$route)
-      deviceReady(function(){
+      meuUtils.deviceReady(function(){
         
         /*
         TODO: Replace by Global service and getCurrentItem, getCurrentCategory methods
         */
         var title = current.$$route.title || $rootScope.items[current.params.id].title || "Screen title missing";
         
-        meuAnalytics.trackView(title);
+        meuCordova.analytics.trackView(title);
       });
     });
 
+    var activate = function() {
+      $log.debug('Api url: ' + meuCloud.API.Config.getProperty('apiUrl'));
+      $log.debug('Api domain: ' + meuCloud.API.Config.getProperty('domain'));
+      
+      meuCloud.API.Site.performance()
+      .then(function(response) {
+        updateDatas(response);
+        if (response.promise)
+          return response.promise;
+      })
+      // If response contains a promise, means is from cache and promise will sync w/ Server
+      .then(function(response) {
+        updateDatas(response)
+      })
+      .catch(function(e) {
+        $exceptionHandler(e);
+      })
+    };
+    
+    var updateDatas = function(response) {
+      var data = response.data;
+
+      meuCloud.syncPerformance(data)
+      .then(function(data) {
+        Site.updateAnalytics();
+        
+        data.logo = meuCloud.getSiteLogoUrl();
+        $rootScope.performance = data;
+      });
+    };
+    
     if (AuthService.isAuthenticated()) {
-      var data = {};
       var defaultLogo = "images/header-color.png";
   
       activate();
- 
-      function activate() {
-        API.Site.performance(
-          function(response) {
-            fulfill(response);
-          },
-          function(response) {}
-        );
-      };
-  
-      function fulfill(response) {
-        updateDatas(response);
-        // if we have a promise, we will use the same current function when it is fulfilled
-        if (response.promise) {
-          response.promise
-          .then(function(response) {
-            fulfill(response);
-          })
-          .catch(function(response) {
-          })
-        }
-      }
-  
-      function updateDatas(response) {
-        var data = response.data;
-
-        MeuAPI.setCategories(data.categories);
-        if (data.site && data.site.analytics_token) {
-          deviceReady(function(){
-            meuAnalytics.startTrackerWithId(data.site.analytics_token);
-          });
-          $log.debug("Own Project Analytics token: " + data.site.analytics_token);       
-        } else {
-          $log.debug("Infomobi Analytics token");
-        }
-        data.logo = data.site.hasOwnProperty("logo") && data.site.logo != "" ? APP.cdnUrl + data.site.logo : defaultLogo;
-        $rootScope.performance = data;
-    
-        angular.copy(data, vm.site);
-      }
     }
   };
 })();
