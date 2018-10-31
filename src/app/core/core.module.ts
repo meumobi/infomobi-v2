972e4ca063156239d9d4e53763205d806e13f66b
Daniel Antonio Conte <danconte72@gmail.com>
2018-10-31 08:21:55 -0300

    feat: add ngx-moment

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicStorageModule } from '@ionic/storage';
 import { HttpClientModule } from '@angular/common/http';
+import { MomentModule } from 'ngx-moment';
 
 @NgModule({
   imports: [
     CommonModule,
     IonicStorageModule.forRoot(),
     HttpClientModule,
+    MomentModule,
+  ],
+  exports: [
+    MomentModule
   ],
   declarations: []
 })
--- src/app/items/pages/item-detail/item-detail.module.ts
+++ src/app/items/pages/item-detail/item-detail.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { ItemDetailPage } from './item-detail.page';
 import { CommentsModule } from '@comments/comments.module';
+import { CoreModule } from '@core/core.module';
 
 const routes: Routes = [
   {
@@ -21,6 +22,7 @@ const routes: Routes = [
     FormsModule,
     CommentsModule,
     IonicModule,
+    CoreModule,
     RouterModule.forChild(routes)
   ],
   declarations: [ItemDetailPage]
