a713cc21544fcbe26eecdf34e1247d395ae8bc97
victor dias <victor.dias@meumobi.com>
2018-11-12 05:15:08 +0000

    refactor: add translateModule on shared

--- src/app/comments/pages/comments-list/comments-list.module.ts
+++ src/app/comments/pages/comments-list/comments-list.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-
-import { IonicModule } from '@ionic/angular';
-
 import { CommentsListPage } from './comments-list.page';
 import { CommentsModule } from 'app/comments/comments.module';
-import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SharedModule } from '@shared/shared.module';
+
 
 const routes: Routes = [
   {
@@ -22,7 +21,7 @@ const routes: Routes = [
     FormsModule,
     CommentsModule,
     IonicModule,
-    TranslateModule,
+    SharedModule,
     RouterModule.forChild(routes)
   ],
   declarations: [CommentsListPage]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    TranslateModule,
+  ],
+  declarations: []
+})
+export class SharedModule { }
